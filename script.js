1ffce874c4c1896fcb4918dd60647d63dfe118c2
Barrett Cope <barrettcope@gmail.com>
2017-05-16 08:40:06 -0400

    Clean up and comment

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+// blocks ======================================================================
+// must have a length that is a square number
+var fourByFour = [
+  ['A','A','E','E','G','N'],
+  ['A','C','H','O','P','S'],
+  ['E','E','G','H','N','W'],
+  ['E','H','R','T','V','W'],
+  ['R','I','L','X','E','D'],
+  ['O','S','S','E','I','T'],
+  ['A','P','F','S','K','F'],
+  ['T','A','O','O','W','T'],
+  ['T','E','L','R','Y','T'],
+  ['O','M','C','I','U','T'],
+  ['D','Y','V','E','R','L'],
+  ['H','L','R','N','Z','N'],
+  ['Qu','M','N','U','I','H'],
+  ['B','O','O','A','J','B'],
+  ['I','S','D','T','Y','T'],
+  ['E','E','I','N','S','U']
+]
+
 // from boggle deluxe - see https://boardgamegeek.com/article/4975223#4975223
-// blocks need to have a length that is a square number
-var blocks = [
+var fiveByFive = [
   ['A','A','A','F','R','S'],
   ['A','A','E','E','E','E'],
   ['A','A','F','I','R','S'],
@@ -9,7 +29,7 @@ var blocks = [
   ['A','E','E','G','M','U'],
   ['A','E','G','M','N','N'],
   ['A','F','I','R','S','Y'],
-  ['B','J','K','Q','X','Z'],
+  ['B','J','K','Qu','X','Z'],
   ['C','C','N','S','T','W'],
   ['C','E','I','I','L','T'],
   ['C','E','I','L','P','T'],
@@ -28,14 +48,17 @@ var blocks = [
   ['O','O','O','T','T','U']
 ]
 
+var board = []
+
+// using fisher yates shuffle: https://bost.ocks.org/mike/shuffle/
 var shuffle = function(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
+  // while there remain elements to shuffle
+  while (currentIndex) {
+    // pick a remaining element
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-    // And swap it with the current element
+    // and swap it with the current element
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
@@ -47,22 +70,103 @@ function createBoard(blocks) {
   var $board = $('#board')
   var orientations = ['up','right','left','down']
   // delete existing letters if they exist
-  $('.letter').remove()
+  $('.letter-container').remove()
+  board = []
   function createRow(array) {
     var $newRow = $('<div></div>')
       .addClass('row')
+    var newRow = []
     for (var i = 0; i < array.length; i++) {
+      // randomly pick a letter from the current block i
       var newLetter = array[i][Math.floor(Math.random() * array[i].length)]
-      var $newDiv = $('<div></div>')
-        .html(newLetter)
+      var $newDiv = $('<div class="letter-container"><div class="letter">' + newLetter + '</div></div>')
         .addClass(orientations[Math.floor(Math.random() * orientations.length)])
-        .addClass('letter')
+      // underline certain letters
+      if (/[MWZ]/.test(newLetter)) {
+        $newDiv.addClass('underline')
+      }
       $newRow.append($newDiv)
+      newRow.push(newLetter)
     }
+    board.push(newRow)
     return $newRow
   }
   blocks = shuffle(blocks)
   for (var j = 0; j < Math.sqrt(blocks.length); j++) {
     $board.append(createRow(blocks.slice(j * Math.sqrt(blocks.length),(j + 1) * Math.sqrt(blocks.length))))
   }
+}
+
+var currentTimer
+
+// TODO: fix bug with resetting timer
+function startTimer(timer) {
+  timer = setInterval(function() {
+    var $timer = $('#timer')
+    var newTime = ($timer.html() || 181) - 1
+    if (newTime > 10) {
+      $timer.html(newTime)
+    } else if (newTime > 0) {
+      $timer.html(newTime).addClass('warning')
+    } else {
+      $timer.html('Game Over').removeClass('warning')
+      clearInterval(timer)
+    }
+  },1000)
+}
+
+// is word in board ============================================================
+// helper functions
+function isLetter(letter,coord,board) {
+  return board[coord[0]][coord[1]] == letter
+}
+
+function sameCoordinate(coord1,coord2) {
+  return coord1[0] == coord2[0] && coord1[1] == coord2[1]
+}
+
+function getSurroundingCoordinates(coord,boardLength) {
+  var surroundingCoordinates = []
+  for (var i = Math.max(coord[0] -1,0); i < Math.min(coord[0] + 2,boardLength); i++) {
+    for (var j = Math.max(coord[1] -1,0); j < Math.min(coord[1] + 2,boardLength); j++) {
+      surroundingCoordinates.push(new Array(i,j))
+    }
+  }
+  return surroundingCoordinates.filter((e) => {
+    return !sameCoordinate(e,coord)
+  })
+}
+
+function getLetterCoordinates(letter,board) {
+  var letterCoordinates = []
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board.length; j++) {
+      if (board[i][j] == letter) {
+        letterCoordinates.push(new Array(i,j))
+      }
+    }
+  }
+  return letterCoordinates
+}
+
+// solver function
+// TODO: refactor and finish
+function testUserWord(word) {
+  word = word.split('')
+  var match = false
+  var matchCoordinates = []
+  for (var i = 0; i < word.length && matchCoordinates.length === 0; i++) {
+    // TODO: refactor and finish
+  }
+
+  // test every letter of the board against the first letter of the word
+  for (var i = 0; i < board.length && !match; i++) {
+    for (var j = 0; j < board[i].length && !match; j++) {
+      if (board[i][j] == word[0]) {
+        matchCoordinates.push([i,j])
+        // TODO: refactor and finish
+      }
+    }
+  }
+  return matchCoordinates
 }
